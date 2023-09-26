b799e07739985885e738278ebc20d8039cfdee70
Brandon <brandonbax2003@gmail.com>
2023-09-26 16:31:28 +0100

    sdf

---
+++ src/main/java/uk/ac/ed/inf/Main.java
@@ -0,0 +1,16 @@
+package uk.ac.ed.inf;
+import uk.ac.ed.inf.ilp.constant.OrderStatus;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        OrderStatus orderStatus = OrderStatus.DELIVERED;
+
+        System.out.println(orderStatus);
+    }
+}
